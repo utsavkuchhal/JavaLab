dcc462fbe792d86454540c985db02823354e1f8d
Utsav Kuchhal <47279585+utsavkuchhal@users.noreply.github.com>
2020-09-22 23:57:02 -0700

    Create SubClassTwo.java

---
+++ SubClassTwo.java
@@ -0,0 +1,15 @@
+
+public class SubClassTwo {
+
+	public SubClassTwo(int n) {
+		System.out.println("From Subclass constructor");
+	}
+
+	public SubClassTwo() {
+		System.out.println("Default constructor");
+	}
+
+	public void methodTwo() {
+		System.out.println("This os from SubClassTwo");
+	}
+}
