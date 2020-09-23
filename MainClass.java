710c2307feebe04bc42f975b678675fbe0f10acd
Utsav Kuchhal <47279585+utsavkuchhal@users.noreply.github.com>
2020-09-22 23:55:56 -0700

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+
+public class MainClass extends SubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainClass mc = new MainClass();
+		mc.method();
+		mc.methodmc();
+		mc.methodTwo();
+//		mc.methodInterface();
+		
+		
+	}
+
+	public void methodmc() {
+		System.out.println("This from main class");
+	}
+
+}
